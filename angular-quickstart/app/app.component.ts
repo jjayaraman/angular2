db3b4acbf204a6f7de5251b0779a71d3b0ea03c8
Jayakumar Jayaraman <india.jai@gmail.com>
2017-02-18 14:22:14 +0000

    angular2 using CLI

--- angular-quickstart/app/app.component.ts
+++ angular-quickstart/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: '<h1>My First Angular App {{title}}</h1>'
 })
-export class AppComponent { }
+export class AppComponent { 
+
+  title : "Hello"
+}
+
